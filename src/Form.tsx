b6553c0d245f4e2e89c1a2abe77378f74969f4b4
marawanthedev <61912691+marawanthedev@users.noreply.github.com>
2024-09-04 23:37:43 +0800

    update: goback to starter code

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,57 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { TextField, Button, Box, Typography } from "@mui/material";
 import axios from "axios";
-import InteractiveProgressBarOverlay from "./InteractiveProgressBar";
-import { countAwaits } from "./countAwaits";
-import SuccessMessage from "./SuccessMessage";
 
 const MyForm: React.FC = () => {
   const [name, setName] = useState("");
   const [complaint, setComplaint] = useState("");
   const [email, setEmail] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [apiRequestsCount, setApiRequestsCount] = useState(0);
-  const [apiRequestsDone, setApiRequestsDone] = useState(0);
-  const [loadingMessage, setLoadingMessage] = useState("Loading....");
-  const [isComplaintSubmitted, setIsComplaintSubmitted] = useState(false);
-
-  const incrementStepsDone = (nextLoadingMessage?: string) => {
-    if (apiRequestsDone + 1 < apiRequestsCount) {
-      setApiRequestsDone((prev) => prev + 1);
-      setLoadingMessage(nextLoadingMessage ?? "");
-    }
-  };
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    setLoading(true);
 
     try {
-      setLoadingMessage("Retriving User information");
       // API Call 1
 
       await axios.get("http://localhost:3001/long-request-1");
-      incrementStepsDone("Intiating Complaint submission");
 
       await axios.get("http://localhost:3001/long-request-2");
-      incrementStepsDone("Confirming Complaint");
 
       // API Call 3
       await axios.get("http://localhost:3001/long-request-3");
-      incrementStepsDone();
     } catch (error) {
       console.error("Error fetching data:", error);
     } finally {
-      setLoading(false);
-      setApiRequestsCount(0);
-      setApiRequestsDone(0);
-      setIsComplaintSubmitted(true);
     }
   };
-
-  useEffect(() => {
-    setApiRequestsCount(countAwaits(handleSubmit));
-  }, []);
 
   return (
     <Box
@@ -65,47 +37,33 @@ const MyForm: React.FC = () => {
         mx: "auto",
       }}
     >
-      {!loading && !isComplaintSubmitted && (
-        <>
-          <Typography variant="h6">Complaint Form</Typography>
-          <TextField
-            label="Name"
-            variant="outlined"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-
-          <TextField
-            label="Email"
-            variant="outlined"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <TextField
-            label="Complaint"
-            variant="outlined"
-            value={complaint}
-            onChange={(e) => setComplaint(e.target.value)}
-            required
-          />
-          <Button type="submit" variant="contained" color="primary">
-            Submit complaint
-          </Button>
-        </>
-      )}
-
-      <InteractiveProgressBarOverlay
-        loading={loading}
-        apiRequestsCount={apiRequestsCount}
-        apiRequestsDone={apiRequestsDone}
-        message={loadingMessage}
+      <Typography variant="h6">Complaint Form</Typography>
+      <TextField
+        label="Name"
+        variant="outlined"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
       />
-      {isComplaintSubmitted && (
-        <SuccessMessage message="Your Complaint is submitted to our management !!" />
-      )}
+
+      <TextField
+        label="Email"
+        variant="outlined"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <TextField
+        label="Complaint"
+        variant="outlined"
+        value={complaint}
+        onChange={(e) => setComplaint(e.target.value)}
+        required
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Submit complaint
+      </Button>
     </Box>
   );
 };
